7bc517b1f389570082451e5427701574787f994b
azazak123 <azazaka2002@gmail.com>
2023-06-01 17:22:50 +0300

    feat(library-service): add returning

--- frontend/src/components/libraryService/borrowingWindow.tsx
+++ frontend/src/components/libraryService/borrowingWindow.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-import { TeachersBorrowing } from "../../model";
-import { Button, Modal, Table } from "react-bootstrap";
+import { BookStatus, TeachersBorrowing } from "../../model";
+import { Button, Dropdown, Modal, Table } from "react-bootstrap";
 
-type ReadonlyBorrowing = TeachersBorrowing & {
+type ReadonlyBorrowing = Omit<TeachersBorrowing, "teacher_card"> & {
   owner: string;
   librarian_name: string;
   book_title: string;
   required_return_date: string | null;
+  card: number;
 };
 
 type Props = {
@@ -26,20 +27,19 @@ function BorrowingWindow({
 }: Props) {
   const [borrowings, setBorrowings] = useState<ReadonlyBorrowing[]>([]);
   const [show, setShow] = useState(showInitial);
+  const [update, setUpdate] = useState({});
 
   useEffect(() => setShow(showInitial), [showInitial]);
-
-  const handleClose = () => closeWindow();
 
   useEffect(() => {
     getBorrowings(isTeacher, card).then((borrowings) =>
       setBorrowings(borrowings)
     );
-  }, [isTeacher, card]);
+  }, [isTeacher, card, update]);
 
   return (
     <>
-      <Modal show={show} onHide={handleClose} centered size="xl">
+      <Modal show={show} onHide={closeWindow} centered size="xl">
         <Modal.Header closeButton>
           <Modal.Title>{owner}</Modal.Title>
         </Modal.Header>
@@ -54,6 +54,7 @@ function BorrowingWindow({
                 <th className="p-3">Borrow date</th>
                 <th className="p-3">Return date</th>
                 <th className="p-3">Required return date</th>
+                <th className="p-3">Return book</th>
               </tr>
             </thead>
             <tbody>
@@ -66,13 +67,71 @@ function BorrowingWindow({
                   <td className="p-3">{borrowing.borrow_date}</td>
                   <td className="p-3">{borrowing.return_date}</td>
                   <td className="p-3">{borrowing.required_return_date}</td>
+                  {!borrowing.return_date ? (
+                    <td className="p-3">
+                      <Dropdown>
+                        <Dropdown.Toggle variant="warning" id="dropdown-basic">
+                          Return
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                          <Dropdown.Item
+                            onClick={() =>
+                              returnBook(
+                                isTeacher,
+                                borrowing,
+                                BookStatus.Excellent
+                              ).then(() => setUpdate({}))
+                            }
+                          >
+                            Excellent
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                            onClick={() =>
+                              returnBook(
+                                isTeacher,
+                                borrowing,
+                                BookStatus.Good
+                              ).then(() => setUpdate({}))
+                            }
+                          >
+                            Good
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                            onClick={() =>
+                              returnBook(
+                                isTeacher,
+                                borrowing,
+                                BookStatus.Satisfactory
+                              ).then(() => setUpdate({}))
+                            }
+                          >
+                            Satisfactory
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                            onClick={() =>
+                              returnBook(
+                                isTeacher,
+                                borrowing,
+                                BookStatus.Unsatisfactory
+                              ).then(() => setUpdate({}))
+                            }
+                          >
+                            Unsatisfactory
+                          </Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </td>
+                  ) : (
+                    ""
+                  )}
                 </tr>
               ))}
             </tbody>
           </Table>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={closeWindow}>
             Close
           </Button>
         </Modal.Footer>
@@ -91,6 +150,51 @@ async function getBorrowings(
   const borrowings = (await borrowingsRes.json()) as ReadonlyBorrowing[];
 
   return borrowings;
+}
+
+async function returnBook(
+  isTeacher: boolean,
+  borrowing: ReadonlyBorrowing,
+  finishStatus: BookStatus
+): Promise<void> {
+  if (isTeacher) {
+    await fetch(
+      `${import.meta.env.VITE_SERVER_URL}/teachers-borrowing/${borrowing.id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: borrowing.id,
+          teacher_card: borrowing.card,
+          librarian: borrowing.librarian,
+          book: borrowing.book,
+          book_status_start: borrowing.book_status_start,
+          book_status_finish: finishStatus,
+          borrow_date: borrowing.borrow_date,
+          return_date: new Date().toISOString().split("T")[0],
+        }),
+      }
+    );
+  } else {
+    await fetch(
+      `${import.meta.env.VITE_SERVER_URL}/students-borrowing/${borrowing.id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: borrowing.id,
+          student_card: borrowing.card,
+          librarian: borrowing.librarian,
+          book: borrowing.book,
+          book_status_start: borrowing.book_status_start,
+          book_status_finish: finishStatus,
+          borrow_date: borrowing.borrow_date,
+          return_date: new Date().toISOString().split("T")[0],
+          required_return_date: borrowing.required_return_date,
+        }),
+      }
+    );
+  }
 }
 
 export default BorrowingWindow;
